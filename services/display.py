212e4e59f127fd5c6a2a76e32e6e3bb76e028630
msmickersgill <msmickersgill@redpoints.com>
2022-04-27 10:05:40 +0200

    Initial commit. Application skeleton and unit tests

---
+++ services/display.py
@@ -0,0 +1,6 @@
+import pandas 
+
+class Display: 
+
+    def __init__(self, data):
+        self.data = data
---
+++ tests/register_test.py
@@ -0,0 +1,34 @@
+import pytest
+import unittest
+
+# TODO: import cash register 
+
+class RegisterTest: 
+
+    def test_empty_cart(): 
+        """Check if cart is initiated with no items"""
+        pass
+
+    def check_CEOs_order():
+        """Check Buy One Get One Free discount is applied to Green Tea and items' total is correct"""
+        pass 
+
+    def check_COOs_order(): 
+        """Check 50c discount is applied to strawberries and items' total is correct"""
+        pass 
+
+    def check_VPs_order(): 
+        """Check coffees are 2/3 of original price and that items' total is correct"""
+        pass 
+
+    def check_bogoff_discount(): 
+        """Check Buy One Get One Free discount is only applied when appropriate"""
+        pass
+
+    def check_strawberries_discount(): 
+        """Check 50c discount is only applied when appropriate"""
+        pass 
+
+    def check_coffee_discount(): 
+        """Check coffees are 2/3 of original price when appropriate"""
+        pass 
